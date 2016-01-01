c16c36470f7933740bfce09d029ad38bd2631f0b
halhorn <halhorn@halmidi.com>
2016-01-01 14:06:03 +0900

    fix Task entity

--- TimeLoggerTests/Entity/TaskTest.swift
+++ TimeLoggerTests/Entity/TaskTest.swift
@@ -7,46 +7,33 @@
 //
 
 import XCTest
+import RealmSwift
 @testable import TimeLogger
 
-class TaskTest: XCTestCase, ITaskDelegate {
-    var changedProperty : TaskProperty?
-    var changedValue : Task?
-
-    override func setUp() {
-        super.setUp()
-        self.changedProperty = nil
-        self.changedValue = nil
-    }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
+class TaskTest: HALTestCase {
+    var token : NotificationToken?
 
     func testProperty() {
-        let task = Task(taskId: 1, groupId: 2, name: "name", createdAt: NSDate(timeIntervalSince1970: 0))
+        let task = Task(value: ["taskId": 1, "groupId": 2, "name": "name", "createdAt": NSDate(timeIntervalSince1970: 0)])
         XCTAssertEqual(task.taskId, 1)
         XCTAssertEqual(task.groupId, 2)
         XCTAssertEqual(task.name, "name")
         XCTAssertEqual(task.createdAt, NSDate(timeIntervalSince1970: 0))
     }
     
-    func testNameChange() {
-        let task = Task(taskId: 1, groupId: 2, name: "name", createdAt: NSDate(timeIntervalSince1970: 0))
-        task.delegate = self;
-        XCTAssertNil(self.changedProperty)
-        XCTAssertNil(self.changedValue)
+    func testStore() {
+        var notificationCalled = false;
+        let task = Task(value: ["taskId": 1, "groupId": 2, "name": "name", "createdAt": NSDate(timeIntervalSince1970: 0)])
         
-        task.name = "newName"
-        XCTAssertEqual(task.name, "newName")
-        XCTAssertEqual(self.changedProperty!, TaskProperty.Name)
-        XCTAssertEqual(self.changedValue!, task)
-        XCTAssertEqual(self.changedValue!.name, "newName")
-    }
-    
-    func didTaskUpdate(property: TaskProperty, task: Task) {
-        self.changedProperty = property
-        self.changedValue = task
+        let realm2 = try! Realm();
+        token = realm2.addNotificationBlock { (notification, realm) -> Void in
+            notificationCalled = true;
+        }
+        let realm = try! Realm();
+        try! realm.write {
+            realm.add(task)
+        }
+        XCTAssertEqual(realm.objects(Task).first, task)
+        XCTAssertTrue(notificationCalled)
     }
 }
