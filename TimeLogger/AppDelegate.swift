558c3b76cd0d9aa6d87707da428d7a6ad18f6dde
halhorn <halhorn@halmidi.com>
2016-01-07 23:12:58 +0900

    use preinstall model

--- TimeLogger/AppDelegate.swift
+++ TimeLogger/AppDelegate.swift
@@ -15,7 +15,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        if (!PreInstallerModel.isPreInstalled()) {
+            do {
+                try PreInstallerModel.install()
+            } catch {}
+        }
         return true
     }
 
